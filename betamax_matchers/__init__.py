72f262f122562177b7b95c4523f9e15d80e444d4
Ian Cordasco <graffatcolmingov@gmail.com>
2014-07-19 13:58:42 -0500

    Initial structure for package

---
+++ betamax_matchers/__init__.py
@@ -0,0 +1,17 @@
+"""
+betamax_matchers
+================
+
+See http://betamax.rtfd.org/ for documentation.
+
+:copyright: (c) 2014 by Ian Cordasco
+:license: Apache 2.0, see LICENSE for more details
+
+"""
+
+__author__ = 'Ian Cordasco'
+__copyright__ = 'Copyright 2014 Ian Cordasco'
+__license__ = 'Apache 2.0'
+__title__ = 'betamax_matchers'
+__version__ = '0.1.0'
+__version_info__ = tuple(int(i) for i in __version__.split('.'))
