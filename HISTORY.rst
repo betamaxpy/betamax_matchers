6e5d8d9f4d3356469c189a0f4e070b44473fbd36
Ian Cordasco <graffatcolmingov@gmail.com>
2015-06-17 19:31:26 -0500

    Cut a release (0.2.0)

---
+++ HISTORY.rst
@@ -0,0 +1,11 @@
+0.2.0 - 2015-06-17
+------------------
+
+- Add ``MultipartFormDataBodyMatcher``
+
+0.1.0 - 2014-08-20
+------------------
+
+- Add ``JSONBodyMatcher``
+
+- Add ``URLEncodedBodyMatcher``
--- betamax_matchers/__init__.py
+++ betamax_matchers/__init__.py
@@ -13,5 +13,5 @@ __author__ = 'Ian Cordasco'
 __copyright__ = 'Copyright 2014 Ian Cordasco'
 __license__ = 'Apache 2.0'
 __title__ = 'betamax_matchers'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 __version_info__ = tuple(int(i) for i in __version__.split('.'))
