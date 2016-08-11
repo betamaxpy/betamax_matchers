30d76841dd5ed8474d3b6374cbd76332d4de5475
Bryce Boe <bbzbryce@gmail.com>
2016-08-10 22:32:16 -0700

    Silence flake8 in relevant compat lines.

--- betamax_matchers/_compat.py
+++ betamax_matchers/_compat.py
@@ -1,4 +1,4 @@
 try:
-    from betamax.cassette import util as betamax_util
+    from betamax.cassette import util as betamax_util  # NOQA
 except ImportError:
-    from betamax import util as betamax_util
+    from betamax import util as betamax_util  # NOQA
